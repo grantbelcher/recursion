8e00ca3eaa6cd96e1110e86d602074035188e0c0
Grant Belcher <gtbelch@gmail.com>
2020-01-16 09:42:56 -0800

    (alternate solution) getElementsByClass

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,27 +6,60 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
 
+	var result = [];
+
 	var body = document.body
 
-	var desiredElements = [];
+	
 
-	var containsClass = function(body){
+	function containsClass (element){
 
-		if (body.classList && body.classList.contains(className)){
-
-			desiredElements.push(body)
-
-		}
-		if (body.childNodes.length > 0){
-
-			_.each(body.childNodes, containsClass)
-		}
 		
+
+
+		if (element.childNodes.length === 0){
+
+			if (element.classList === undefined){
+
+				
+
+			} else if (element.classList.contains(className)){
+				console.log('pushed', element)
+				result.push(element)
+			}
+
+
+		}
+
+
+
+
+
+		else if (element.childNodes.length > 0){
+
+			if(element.classList === undefined || !element.classList.contains(className)){
+
+				_.each(element.childNodes, containsClass)
+
+			} else if(element.classList.contains(className)){
+			console.log('pushed', element)
+			result.push(element)
+			_.each(element.childNodes, containsClass)
+		}
+		}
+
+
+		// else if (element.childNodes.length > 0){
+		// 	_.each(element.childNodes, containsClass)
+		// }
+
+
 	}
 
+	
 	containsClass(body)
 
-	return desiredElements
+	return result
 
 };
 
